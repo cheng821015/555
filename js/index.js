8519725a600da37c30d3569dc5dcb81f0a578640
cheng821015 <wl02856836@gmail.com>
2019-08-31 00:42:14 +0800

    2019/08/31 新增code

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var controller = new ScrollMagic.Controller();
+var animation_02 = TweenMax.staggerFromTo('.mpa-item', 1, {
+    y: -20,
+    alpha: 0
+}, {
+    y: 0,
+    alpha: 1
+}, .2)
+
+
+var section_02 = new ScrollMagic.Scene({
+    triggerElement: "#container_01",
+    reverse: true,
+    offset: 0,
+
+}).setTween(animation_02)
+.addIndicators({
+    colorTigger: "white",
+    colorStart: "white",
+})
+.addTo(controller)
+
