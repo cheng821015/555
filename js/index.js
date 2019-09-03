b662b4bdbd9beea2c2df6a6a33bed1ff54e69036
cheng821015 <wl02856836@gmail.com>
2019-09-03 20:58:44 +0800

    2019/09/03 最後修改

--- js/index.js
+++ js/index.js
@@ -1,5 +1,50 @@
 var controller = new ScrollMagic.Controller();
-var animation_02 = TweenMax.staggerFromTo('.mpa-item', 1, {
+
+
+
+
+//最新消息動畫
+var animation_02 = TweenMax.staggerFromTo('.message_1', 1, {
+    x: -40,
+    alpha: 0
+}, {
+    x: 0,
+    alpha: 1
+}, .2)
+
+
+var section_02 = new ScrollMagic.Scene({
+    triggerElement: "#container_02",
+    reverse: false,
+    offset: 0,
+
+}).setTween(animation_02)
+// .addIndicators()
+.addTo(controller)
+
+var animation_02 = TweenMax.staggerFromTo('.message_2', 1, {
+    x: 40,
+    alpha: 0
+}, {
+    x: 0,
+    alpha: 1
+}, .2)
+
+
+var section_02 = new ScrollMagic.Scene({
+    triggerElement: "#container_02",
+    reverse: false,
+    offset: 0,
+
+}).setTween(animation_02)
+// .addIndicators()
+.addTo(controller)
+
+
+
+
+//店鋪資訊動畫
+var animation_01 = TweenMax.staggerFromTo('.mpa-item', 1, {
     y: -20,
     alpha: 0
 }, {
@@ -10,10 +55,10 @@ var animation_02 = TweenMax.staggerFromTo('.mpa-item', 1, {
 
 var section_02 = new ScrollMagic.Scene({
     triggerElement: "#container_01",
-    reverse: true,
+    reverse: false,//只要執行一次動畫ture
     offset: 0,
 
-}).setTween(animation_02)
+}).setTween(animation_01)
 // .addIndicators()
 .addTo(controller)
 
